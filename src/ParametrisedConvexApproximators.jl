6d3163632be2da51851d9d99b6320610bfebaa41
NPLawrence <61165981+NPLawrence@users.noreply.github.com>
2022-10-27 15:33:07 -0700

    remove SCS, Optim, Convex

--- src/ParametrisedConvexApproximators.jl
+++ src/ParametrisedConvexApproximators.jl
@@ -2,9 +2,9 @@ module ParametrisedConvexApproximators
 
 using Flux
 using UnPack
-using Convex
-using SCS  # 211207; currently not compatible with apple silicon
-using Optim
+# using Convex
+# using SCS  # 211207; currently not compatible with apple silicon
+# using Optim
 using Transducers
 using Random
 
@@ -14,14 +14,14 @@ export AbstractApproximator, FNN
 export ConvexApproximator, MA, LSE
 export ParametrisedConvexApproximator, PMA, PLSE
 export PICNN, project_nonnegative!
-export optimise, number_of_parameters
+# export optimise, number_of_parameters
 # data manipulation
 export partitionTrainTest
 
 
 # approximators
 include("approximators/approximators.jl")
-include("optimise.jl")
+# include("optimise.jl")
 include("data_manipulation/data_manipulation.jl")
 
 
